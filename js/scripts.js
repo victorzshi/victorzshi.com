92ce6c93e684b6903271875fc946f1b650aee270
Victor Z. Shi <vzshi@pm.me>
2022-01-23 17:13:33 -0500

    Render staggered grid of squares

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,30 @@
 window.onload = function () {
+  var GRID_WIDTH = 20;
+  var GRID_HEIGHT = 10;
+
+  var totalElements = GRID_WIDTH * GRID_HEIGHT;
+
+  var gridEl = document.querySelector('.grid');
+  var gridFragment = document.createDocumentFragment();
+
+  for (var i = 0; i < totalElements; i++) {
+    var squareEl = document.createElement('div');
+    squareEl.classList.add('square');
+    gridFragment.appendChild(squareEl);
+  }
+
+  gridEl.appendChild(gridFragment);
+
+  anime({
+    targets: '.grid .square',
+    scale: [
+      { value: 0, easing: 'easeOutSine', duration: 500 },
+      { value: 0.5, easing: 'easeInOutQuad', duration: 250 },
+      { value: 0, easing: 'easeOutSine', duration: 250 },
+    ],
+    delay: anime.stagger(200, { grid: [GRID_WIDTH, GRID_HEIGHT], from: 'center', direction: 'reverse'})
+  });
+
   anime({
     targets: 'svg path',
     strokeDashoffset: [anime.setDashoffset, 0],
