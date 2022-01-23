866fff2d118dbed8bd01246442e84345740680ba
Victor Z. Shi <vzshi@pm.me>
2022-01-23 17:40:09 -0500

    Add grid (#2)
    * Render staggered grid of squares
    
    * Render grid and logo in timeline

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,31 @@
 window.onload = function () {
-  anime({
+  var GRID_WIDTH = 20;
+  var GRID_HEIGHT = 10;
+
+  var totalElements = GRID_WIDTH * GRID_HEIGHT;
+
+  var gridEl = document.querySelector('.grid');
+  var gridFragment = document.createDocumentFragment();
+
+  for (var i = 0; i < totalElements; i++) {
+    var squareEl = document.createElement('div');
+    squareEl.classList.add('square');
+    gridFragment.appendChild(squareEl);
+  }
+
+  gridEl.appendChild(gridFragment);
+
+  var animeGrid = {
+    targets: '.grid .square',
+    scale: [
+      { value: 0, easing: 'easeOutSine', duration: 500 },
+      { value: 0.5, easing: 'easeInOutQuad', duration: 250 },
+      { value: 0, easing: 'easeOutSine', duration: 250 },
+    ],
+    delay: anime.stagger(200, { grid: [GRID_WIDTH, GRID_HEIGHT], from: 'center', direction: 'reverse' })
+  };
+
+  var animeLogo = {
     targets: 'svg path',
     strokeDashoffset: [anime.setDashoffset, 0],
     easing: 'easeInOutSine',
@@ -7,5 +33,9 @@ window.onload = function () {
     delay: function (el, i) { return i * 250 },
     direction: 'alternate',
     loop: false
-  });
+  };
+
+  var tl = anime.timeline();
+
+  tl.add(animeGrid).add(animeLogo);
 }
