19446d409179ba5d0858364c30574ed19d407311
Victor Z. Shi <vzshi@pm.me>
2022-01-23 19:14:34 -0500

    Dynamically render rows and columns (#3)

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,65 @@
 window.onload = function () {
-  var GRID_WIDTH = 20;
-  var GRID_HEIGHT = 10;
+  const viewportWidth = window.innerWidth;
+  const viewportHeight = window.innerHeight;
 
-  var totalElements = GRID_WIDTH * GRID_HEIGHT;
+  const MAX_GRID_SIZE = 20;
+  let rows = 0;
+  let columns = 0;
 
-  var gridEl = document.querySelector('.grid');
-  var gridFragment = document.createDocumentFragment();
+  if (viewportWidth > viewportHeight) {
+    columns = MAX_GRID_SIZE;
+    const squareHeight = viewportWidth / columns;
+    rows = Math.ceil(viewportHeight / squareHeight);
+  }
+  else {
+    rows = MAX_GRID_SIZE;
+    const squareWidth = viewportHeight / rows;
+    columns = Math.ceil(viewportWidth / squareWidth);
+  }
 
-  for (var i = 0; i < totalElements; i++) {
-    var squareEl = document.createElement('div');
+  const totalElements = rows * columns;
+  const gridEl = document.querySelector('.grid');
+  const gridFragment = document.createDocumentFragment();
+
+  for (let i = 0; i < totalElements; i++) {
+    const squareEl = document.createElement('div');
     squareEl.classList.add('square');
     gridFragment.appendChild(squareEl);
   }
 
   gridEl.appendChild(gridFragment);
 
-  var animeGrid = {
+  const animeGrid = {
     targets: '.grid .square',
     scale: [
-      { value: 0, easing: 'easeOutSine', duration: 500 },
-      { value: 0.5, easing: 'easeInOutQuad', duration: 250 },
+      { value: 0.1, duration: 0 },
+      { value: 0, easing: 'easeOutBounce', duration: 500 },
+      { value: 0.4, easing: 'easeInOutQuad', duration: 250 },
       { value: 0, easing: 'easeOutSine', duration: 250 },
     ],
-    delay: anime.stagger(200, { grid: [GRID_WIDTH, GRID_HEIGHT], from: 'center', direction: 'reverse' })
+    delay: anime.stagger(200, { grid: [columns, rows], from: 'center', direction: 'reverse' })
   };
 
-  var animeLogo = {
-    targets: 'svg path',
+  const animeOutlineLogo = {
+    targets: '.outline-logo path',
     strokeDashoffset: [anime.setDashoffset, 0],
     easing: 'easeInOutSine',
     duration: 3000,
     delay: function (el, i) { return i * 250 },
     direction: 'alternate',
-    loop: false
   };
 
-  var tl = anime.timeline();
+  const animeFilledLogo = {
+    targets: '.filled-logo path',
+    opacity: [0, 1],
+    easing: 'easeOutSine',
+    duration: 1000
+  };
 
-  tl.add(animeGrid).add(animeLogo);
+  const tl = anime.timeline();
+
+  tl
+    .add(animeGrid)
+    .add(animeOutlineLogo)
+    .add(animeFilledLogo, '-=2000');
 }
