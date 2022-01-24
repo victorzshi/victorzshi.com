f50c1571fe5252b568833e22547ff320a99aba3d
Victor Z. Shi <vzshi@pm.me>
2022-01-23 19:57:26 -0500

    Fix square sizes on mobile screens

--- js/scripts.js
+++ js/scripts.js
@@ -6,60 +6,71 @@ window.onload = function () {
   let rows = 0;
   let columns = 0;
 
-  if (viewportWidth > viewportHeight) {
+  const isLandscape = viewportWidth > viewportHeight ? true : false;
+
+  if (isLandscape) {
     columns = MAX_GRID_SIZE;
     const squareHeight = viewportWidth / columns;
-    rows = Math.ceil(viewportHeight / squareHeight);
-  }
-  else {
+    rows = Math.floor(viewportHeight / squareHeight);
+  } else {
     rows = MAX_GRID_SIZE;
     const squareWidth = viewportHeight / rows;
-    columns = Math.ceil(viewportWidth / squareWidth);
+    columns = Math.floor(viewportWidth / squareWidth);
   }
 
   const totalElements = rows * columns;
-  const gridEl = document.querySelector('.grid');
+  const gridEl = document.querySelector(".grid");
   const gridFragment = document.createDocumentFragment();
 
   for (let i = 0; i < totalElements; i++) {
-    const squareEl = document.createElement('div');
-    squareEl.classList.add('square');
+    const squareEl = document.createElement("div");
+    squareEl.classList.add("square");
+
+    if (isLandscape) {
+      squareEl.classList.add("landscape");
+    } else {
+      squareEl.classList.add("portrait");
+    }
+
     gridFragment.appendChild(squareEl);
   }
 
   gridEl.appendChild(gridFragment);
 
   const animeGrid = {
-    targets: '.grid .square',
+    targets: ".grid .square",
     scale: [
       { value: 0.1, duration: 0 },
-      { value: 0, easing: 'easeOutBounce', duration: 500 },
-      { value: 0.4, easing: 'easeInOutQuad', duration: 250 },
-      { value: 0, easing: 'easeOutSine', duration: 250 },
+      { value: 0, easing: "easeOutBounce", duration: 500 },
+      { value: 0.4, easing: "easeInOutQuad", duration: 250 },
+      { value: 0, easing: "easeOutSine", duration: 250 },
     ],
-    delay: anime.stagger(200, { grid: [columns, rows], from: 'center', direction: 'reverse' })
+    delay: anime.stagger(200, {
+      grid: [columns, rows],
+      from: "center",
+      direction: "reverse",
+    }),
   };
 
   const animeOutlineLogo = {
-    targets: '.outline-logo path',
+    targets: ".outline-logo path",
     strokeDashoffset: [anime.setDashoffset, 0],
-    easing: 'easeInOutSine',
+    easing: "easeInOutSine",
     duration: 3000,
-    delay: function (el, i) { return i * 250 },
-    direction: 'alternate',
+    delay: function (el, i) {
+      return i * 250;
+    },
+    direction: "alternate",
   };
 
   const animeFilledLogo = {
-    targets: '.filled-logo path',
+    targets: ".filled-logo path",
     opacity: [0, 1],
-    easing: 'easeOutSine',
-    duration: 1000
+    easing: "easeOutSine",
+    duration: 1000,
   };
 
   const tl = anime.timeline();
 
-  tl
-    .add(animeGrid)
-    .add(animeOutlineLogo)
-    .add(animeFilledLogo, '-=2000');
-}
+  tl.add(animeGrid).add(animeOutlineLogo).add(animeFilledLogo, "-=2000");
+};
